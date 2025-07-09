78ac815d6860a9aa5606bb553ce8e9c6bbdd7ffd
nanasarpp <nanasarpp@gmail.com>
2025-07-09 14:20:29 +0000

    The future age calculator was added

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,8 +1,8 @@
 #Taking input from user 
-age = int(input("What is your current age "))
+age = input("What is your current age ")
 
 #manipulating future age 
-future_age = age + 27
+future_age = int(age) + 27
 
 #displaying the future age 
 print(f"In 2050, you will be {future_age} years old")
