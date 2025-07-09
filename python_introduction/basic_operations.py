cf28a4911aec6f2b535ec57c67061b482ca67bea
nanasarpp <nanasarpp@gmail.com>
2025-07-09 12:57:22 +0000

    Add basic arithmetic operations script

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#declaring the variable
+number1 = 10
+number2 = 5 
+
+#finding the sum 
+sum = number1 + number2
+print(f"Addition of {number1} and {number2} is {sum}")
+
+#finding the differnce 
+differnce = number1 - number2
+print(f"Subtraction of {number1} and {number2} is {differnce}")
+
+#finding the product 
+product  = number1 + number2
+print(f"Multiplication of {number1} and {number2} is {product}")
