e27d5f453c742fc80647d32dfdb3316fc849c0f2
nanasarpp <nanasarpp@gmail.com>
2025-07-09 13:09:10 +0000

    I resolved the operation problem

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -11,5 +11,5 @@ differnce = number1 - number2
 print(f"Subtraction of {number1} and {number2} is {differnce}")
 
 #finding the product 
-product  = number1 + number2
+product  = number1 * number2
 print(f"Multiplication of {number1} and {number2} is {product}")
