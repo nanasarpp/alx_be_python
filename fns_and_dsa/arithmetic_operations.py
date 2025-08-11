c0abbd342ed8af5b6497fdc859afbf84aeaea60e
nanasarpp <nanasarpp@gmail.com>
2025-08-11 13:47:32 +0000

    slight changes

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -6,10 +6,11 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if num2 != 0:
-            return num1/num2
+        if num2 == 0:
+             return "Cant perform operation"
+            
         else:
-            return "Cant perform operation"
+           return num1/num2
     else:
         return "Enter a valid operation"
     
