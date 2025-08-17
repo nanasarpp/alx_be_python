14bd01b9a1975bfbb26cf9e7356bc42a26cd84bf
nanasarpp <nanasarpp@gmail.com>
2025-08-17 17:25:58 +0000

    First chance

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount():
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self,amount):
+        return amount + self.account_balance 
+    
+    def withdraw(self,amount):
+        if self.account_balance > amount:
+            self.account_balance - amount
+            return True
+        else:
+            return False
+        
+    def display_balance(self):
+        print( self.account_balance)
