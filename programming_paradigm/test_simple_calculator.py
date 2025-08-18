0670f38d9cf7147b609a3027d9b0b100a4ba4cd9
nanasarpp <nanasarpp@gmail.com>
2025-08-18 08:25:48 +0000

    almost there

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,7 +11,7 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1,1), 0)
         self.assertEqual(self.calc.add(2,0), 2)
     
-    def test_subtract(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(3,4), -1)
 
         self.assertEqual(self.calc.subtract(5,2), 3)
